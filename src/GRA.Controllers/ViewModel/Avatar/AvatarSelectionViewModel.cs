25c860a1ed2704c7807e68049b34934f86460d96
Dan <DanielWilcox@mcldaz.org>
2016-12-20 16:28:59 -0700

    Removed unused usings

--- src/GRA.Controllers/ViewModel/Avatar/AvatarSelectionViewModel.cs
+++ src/GRA.Controllers/ViewModel/Avatar/AvatarSelectionViewModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace GRA.Controllers.ViewModel.Avatar
+﻿namespace GRA.Controllers.ViewModel.Avatar
 {
     public class AvatarSelectionViewModel
     {
