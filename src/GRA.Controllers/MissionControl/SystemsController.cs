b7add9f4e84ae6ae6057613a984f5b5bc217de65
Dan <danielwilcox@mcldaz.org>
2017-08-09 13:21:24 -0700

    Add system and branch backend methods

---
+++ src/GRA.Controllers/MissionControl/SystemsController.cs
@@ -0,0 +1,30 @@
+﻿using GRA.Domain.Model;
+using GRA.Domain.Model.Filters;
+using GRA.Domain.Service;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Logging;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GRA.Controllers.MissionControl
+{
+    [Area("MissionControl")]
+    [Authorize(Policy = Policy.ManageSystems)]
+    public class SystemsController : Base.MCController
+    {
+        private readonly ILogger<SystemsController> _logger;
+        private readonly SiteService _siteService;
+
+        public SystemsController(ILogger<SystemsController> logger,
+            ServiceFacade.Controller context,
+            SiteService siteService) : base(context)
+        {
+            _logger = Require.IsNotNull(logger, nameof(logger));
+            _siteService = Require.IsNotNull(siteService, nameof(siteService));
+            PageTitle = "System";
+        }
+    }
+}
